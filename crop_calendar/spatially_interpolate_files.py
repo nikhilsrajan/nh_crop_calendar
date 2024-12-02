6d28b6182e9a84c11aea31fc52ee22d1c0840923
Nikhil Sasi Rajan <nikhilsasirajan@gmail.com>
2024-12-02 16:25:43 +0100

    debug prints

--- crop_calendar/spatially_interpolate_files.py
+++ crop_calendar/spatially_interpolate_files.py
@@ -289,6 +289,10 @@ def write_out_stack(
 
     years = metadata['years']
     days = metadata['days']
+
+    print(f"DEBUG: metadata['years'] = {metadata['years']}")
+    print(f"DEBUG: metadata['days'] = {metadata['days']}")
+
     out_meta = metadata['geotiff_metadata']
     
     if n_years != len(years):
